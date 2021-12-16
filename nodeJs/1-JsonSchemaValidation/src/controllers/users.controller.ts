80b513c2c7f837823af133c89bb5a1a1130624e4
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-12-16 15:00:40 -0300

    save

--- nodeJs/1-JsonSchemaValidation/src/controllers/users.controller.ts
+++ nodeJs/1-JsonSchemaValidation/src/controllers/users.controller.ts
@@ -15,4 +15,17 @@ UsersController.get('/', async (req: Request, res: Response) => {
 	}
 })
 
+UsersController.post('/create', async (req: Request, res: Response) => {
+	try {
+		res.status(200).send({
+			message: 'Get users with success',
+			users: [],
+		})
+	} catch (e) {
+		res.status(500).send({
+			message: 'Contact the administrator',
+		})
+	}
+})
+
 export default UsersController
