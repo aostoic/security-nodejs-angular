f3a9c0993ece6a19e86a748007c49bb8ed2a5618
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-12-17 11:53:06 -0300

    save

--- nodeJs/1-JsonSchemaValidation/src/controllers/users.controller.ts
+++ nodeJs/1-JsonSchemaValidation/src/controllers/users.controller.ts
@@ -4,7 +4,33 @@ import validateFields from '../middlewares/validate-fields.middleware'
 
 const UsersController: Router = Router()
 
-UsersController.post('/create', [
+UsersController.get('/', async (_: Request, res: Response) => {
+	try {
+		res.status(200).send({
+			message: 'Getting users with success',
+			users: [],
+		})
+	} catch (e) {
+		res.status(500).send({
+			message: 'Contact the administrator',
+		})
+	}
+})
+
+UsersController.get('/:id', async (_: Request, res: Response) => {
+	try {
+		res.status(200).send({
+			message: 'Getting user {ID} with success',
+			users: [],
+		})
+	} catch (e) {
+		res.status(500).send({
+			message: 'Contact the administrator',
+		})
+	}
+})
+
+UsersController.post('/', [
 	check('name', 'Name field is required').not().isEmpty(),
 	check('name', 'Name field must be an string').isString(),
 	check('age', 'Age field is required').not().isEmpty(),
@@ -14,6 +40,32 @@ UsersController.post('/create', [
 	try {
 		res.status(200).send({
 			message: 'User created with success',
+			users: [],
+		})
+	} catch (e) {
+		res.status(500).send({
+			message: 'Contact the administrator',
+		})
+	}
+})
+
+UsersController.put('/:id', async (_: Request, res: Response) => {
+	try {
+		res.status(200).send({
+			message: 'Updating user {ID} with success',
+			users: [],
+		})
+	} catch (e) {
+		res.status(500).send({
+			message: 'Contact the administrator',
+		})
+	}
+})
+
+UsersController.delete('/:id', async (_: Request, res: Response) => {
+	try {
+		res.status(200).send({
+			message: 'Removing user {ID} with success',
 			users: [],
 		})
 	} catch (e) {
